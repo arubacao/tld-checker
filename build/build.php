d681368aa60782bc153465d515bda9ab41cff220
arubacao <arubacao@gmail.com>
2017-09-18 21:41:30 +0200

    implement build script

---
+++ build/build.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Compile Root Zone Database source file.
+ *
+ * This build script will download Root Zone Database
+ * and reformat it in a way that is friendlier to use.
+ *
+ * This script should be run via php-cli.
+ *
+ *
+ * @package arubacao/tld-checker
+ * @author    Christopher Lass <arubacao@gmail.com>
+ */
+
+$start = microtime(true);
+
+define('ROOT_ZONE_DATABASE', 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt');
+define('ROOT_ZONE_DATABASE_MD5', 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt.md5');
+
+define('TEMPLATE', dirname(__FILE__) . '/data.template');
+define('OUTPUT_FILE', dirname(__FILE__) . './../src/RootZoneDatabase.php');
+
+print("Get Root Zone Database\n");
+$root_zone_data = file_get_contents(ROOT_ZONE_DATABASE);
+$root_zone_data_md5 = explode(" ", file_get_contents(ROOT_ZONE_DATABASE_MD5))[0];
+
+print("Validating data\n");
+if (md5($root_zone_data) !== $root_zone_data_md5) {
+    throw new \Exception("Invalid MD5 hash\n");
+}
+
+$tlds = explode("\n", $root_zone_data);
+$tlds = array_filter($tlds,function($tld) {
+    return $tld != '';
+});
+$root_zone_data_version = array_shift($tlds);
+
+if(count($tlds) <= 1500) {
+    throw new \Exception("TLDs array is too small. Something must went wrong.\n");
+}
+if (0 !== strpos($root_zone_data_version, '#')) {
+    throw new \Exception("Version does not match typical pattern. Something must went wrong.\n");
+}
+
+$replacements = array(
+    '%GENERATED%' => date('Y-m-d H:i:s'),
+    '%VERSION%' => $root_zone_data_version,
+    '%SUFFIXES%' => var_export($tlds, true),
+);
+$output = file_get_contents(TEMPLATE);
+$output = str_replace(array_keys($replacements), array_values($replacements), $output);
+
+print("Storing new database\n");
+file_put_contents(OUTPUT_FILE, $output);
+
+$end = microtime(true);
+printf("Time used in seconds: %f\n", $end - $start);
