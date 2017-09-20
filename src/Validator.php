052fc6ae907292bcf43e00b6451d092865140fb2
arubacao <arubacao@gmail.com>
2017-09-21 01:46:54 +0200

    add validation logic

---
+++ src/Validator.php
@@ -0,0 +1,36 @@
+<?php
+namespace Arubacao\TldChecker;
+
+class Validator
+{
+    /**
+     * Checks if value is valid tld.
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public static function isTld($value)
+    {
+        $value = idn_to_ascii($value);
+        $value = strtoupper($value);
+
+        if (in_array($value, RootZoneDatabase::TLDS)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks if value is valid tld.
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public static function endsWithTld($value)
+    {
+        $parts = explode(".", $value);
+        $end = end($parts);
+
+        return self::isTld($end);
+    }
+}
