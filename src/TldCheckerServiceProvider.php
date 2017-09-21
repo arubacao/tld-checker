27cb8a807a5f335624e2e150b9ef525e46fe8a72
arubacao <arubacao@gmail.com>
2017-09-21 03:13:46 +0200

    implement laravel service provider

---
+++ src/TldCheckerServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christopher
+ * Date: 18.09.2017
+ * Time: 22:02
+ */
+
+namespace Arubacao\TldChecker;
+
+use Validator;
+use Illuminate\Support\ServiceProvider;
+use Arubacao\TldChecker\Validator as TldValidator;
+
+class TldCheckerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Validator::extend(
+            'is_tld',
+            function ($attr, $value, $param, $validator) {
+                return TldValidator::isTld($value);
+            },
+            'The :attribute is not a valid tld.'
+        );
+
+        Validator::extend(
+            'ends_with_tld',
+            function ($attr, $value, $param, $validator) {
+                return TldValidator::endsWithTld($value);
+            },
+            'The :attribute does not end with a valid tld.'
+        );
+    }
+}
