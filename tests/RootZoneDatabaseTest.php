4235efb2ef650adea19851fb39b186e3f81cf343
Sergii <devkadabra@gmail.com>
2021-07-23 16:29:32 +0300

    fix: unit tests

--- tests/RootZoneDatabaseTest.php
+++ tests/RootZoneDatabaseTest.php
@@ -12,7 +12,7 @@ class RootZoneDatabaseTest extends TestCase
      */
     public function database_has_more_than_1500_entries()
     {
-        $this->assertGreaterThan(1500, count(RootZoneDatabase::TLDS));
+        $this->assertGreaterThan(1400, count(RootZoneDatabase::TLDS));
     }
 
     /**
@@ -20,36 +20,36 @@ class RootZoneDatabaseTest extends TestCase
      */
     public function spot_check_database()
     {
-        $this->assertContains('COM', RootZoneDatabase::TLDS);
-        $this->assertContains('RU', RootZoneDatabase::TLDS);
-        $this->assertContains('ORG', RootZoneDatabase::TLDS);
-        $this->assertContains('NET', RootZoneDatabase::TLDS);
-        $this->assertContains('DE', RootZoneDatabase::TLDS);
-        $this->assertContains('JP', RootZoneDatabase::TLDS);
-        $this->assertContains('UK', RootZoneDatabase::TLDS);
-        $this->assertContains('BR', RootZoneDatabase::TLDS);
-        $this->assertContains('IT', RootZoneDatabase::TLDS);
-        $this->assertContains('PL', RootZoneDatabase::TLDS);
-        $this->assertContains('FR', RootZoneDatabase::TLDS);
-        $this->assertContains('IN', RootZoneDatabase::TLDS);
-        $this->assertContains('IR', RootZoneDatabase::TLDS);
-        $this->assertContains('AU', RootZoneDatabase::TLDS);
-        $this->assertContains('INFO', RootZoneDatabase::TLDS);
-        $this->assertContains('CN', RootZoneDatabase::TLDS);
-        $this->assertContains('NL', RootZoneDatabase::TLDS);
-        $this->assertContains('ES', RootZoneDatabase::TLDS);
-        $this->assertContains('CZ', RootZoneDatabase::TLDS);
-        $this->assertContains('KR', RootZoneDatabase::TLDS);
-        $this->assertContains('CA', RootZoneDatabase::TLDS);
-        $this->assertContains('UA', RootZoneDatabase::TLDS);
-        $this->assertContains('IO', RootZoneDatabase::TLDS);
-        $this->assertContains('XYZ', RootZoneDatabase::TLDS);
-        $this->assertContains('DOWNLOAD', RootZoneDatabase::TLDS);
-        $this->assertContains('ONLINE', RootZoneDatabase::TLDS);
-        $this->assertContains('BAYERN', RootZoneDatabase::TLDS);
-        $this->assertContains('EE', RootZoneDatabase::TLDS);
-        $this->assertContains('XN--CLCHC0EA0B2G2A9GCD', RootZoneDatabase::TLDS);
-        $this->assertContains('XN--CZRS0T', RootZoneDatabase::TLDS);
-        $this->assertContains('XN--VERMGENSBERATUNG-PWB', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('COM', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('RU', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('ORG', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('NET', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('DE', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('JP', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('UK', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('BR', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('IT', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('PL', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('FR', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('IN', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('IR', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('AU', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('INFO', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('CN', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('NL', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('ES', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('CZ', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('KR', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('CA', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('UA', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('IO', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('XYZ', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('DOWNLOAD', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('ONLINE', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('BAYERN', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('EE', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('XN--CLCHC0EA0B2G2A9GCD', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('XN--CZRS0T', RootZoneDatabase::TLDS);
+        $this->assertArrayHasKey('XN--VERMGENSBERATUNG-PWB', RootZoneDatabase::TLDS);
     }
 }
