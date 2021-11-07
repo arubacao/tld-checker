b0c68bed9e4d3ad1f235bc3f63b439e5747a2824
arubacao <arubacao@users.noreply.github.com>
2021-11-07 04:01:39 +0000

    Apply fixes from StyleCI

--- src/RootZoneDatabase.php
+++ src/RootZoneDatabase.php
@@ -20,6 +20,7 @@ namespace Arubacao\TldChecker;
  * @see https://data.iana.org/TLD/tlds-alpha-by-domain.txt
  *
  * # Version 2021110600, Last Updated Sat Nov  6 07:07:01 2021 UTC
+ *
  * @version 2021110600
  */
 class RootZoneDatabase
