f3ddc3a925c2df64fccc769fcdee84ebd3aaa3c6
arubacao <arubacao@gmail.com>
2024-09-01 04:01:16 +0000

    update iana db - Sun Sep  1 04:01:16 UTC 2024

--- src/RootZoneDatabase.php
+++ src/RootZoneDatabase.php
@@ -19,13 +19,12 @@ namespace Arubacao\TldChecker;
  *
  * @see https://data.iana.org/TLD/tlds-alpha-by-domain.txt
  *
- * # Version 2024082400, Last Updated Sat Aug 24 07:07:02 2024 UTC
- *
- * @version 2024082400
+ * # Version 2024083100, Last Updated Sat Aug 31 07:07:01 2024 UTC
+ * @version 2024083100
  */
 class RootZoneDatabase
 {
-    const VERSION = 2024082400;
+    const VERSION = 2024083100;
 
     const TLDS = [
         'AAA' => null,
